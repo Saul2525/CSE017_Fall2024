22bf3fb9f7edd9760c9550f934c1cc72b2ba6727
Saul Toribio <sat727@lehigh.edu>
2024-09-02 18:21:39 -0400

    Base code finished.

--- ALAs/ALA_1/Faculty.java
+++ ALAs/ALA_1/Faculty.java
@@ -6,7 +6,27 @@
 */
 
 public class Faculty extends Employee {
+    private String rank;
+
     public Faculty() {
-        
+        super();
+        rank = "None";
+    }
+
+    public Faculty(int id, String name, String address, String phone, String email, String position, double salary, String rank) {
+        super(id, name, address, phone, email, position, salary);
+        this.rank = rank;
+    }
+
+    public String getRank() {
+        return rank;
+    }
+
+    public String toString() {
+        return (super.toString() + String.format("Rank: %s\n", rank));
+    }
+
+    public void setRank(String rank) {
+        this.rank = rank;
     }
 }
