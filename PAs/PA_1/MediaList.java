d4cb282236e43e40cd20f156101c8aa074ad5ea4
Saul Toribio <sat727@lehigh.edu>
2024-09-03 16:03:15 -0400

    What message?

---
+++ PAs/PA_1/MediaList.java
@@ -0,0 +1,13 @@
+/*
+    Saul Toribio
+    9/3/24
+    CSE017 Fall 2024: PA 1
+    IDE: VSCode; JDK: 11
+*/
+
+package PAs.PA_1;
+
+public class MediaList extends Media {
+    private Media[] media = new Media[10];
+    private int count;
+}
