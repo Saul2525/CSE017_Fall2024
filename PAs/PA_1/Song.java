0a085f4bd55315e1a780e3495873d8a1517e6f08
Saul Toribio <sat727@lehigh.edu>
2024-09-03 13:03:25 -0400

    What message?

---
+++ PAs/PA_1/Song.java
@@ -0,0 +1,30 @@
+/*
+    Saul Toribio
+    9/3/24
+    CSE017 Fall 2024: PA 1
+    IDE: VSCode; JDK: 11
+*/
+
+package PAs.PA_1;
+
+public class Song extends Media {
+    private String artist;
+
+    public Song() {
+        super();
+        artist = "None";
+    }
+
+    public Song(String title, String category, int year, long size, String artist) {
+        super(title, category, year, size);
+        this.artist = artist;
+    }
+
+    public String getArtist() {
+        return artist;
+    }
+
+    public String toString() {
+        return (super.toString() + String.format(""));
+    }
+}
