c8b0dee5ce5f4a4854d99c7eb7b52f0f217adc96
Saul Toribio <sat727@lehigh.edu>
2024-09-03 10:32:25 -0400

    New file.

---
+++ PAs/PA_1/Media.java
@@ -0,0 +1,54 @@
+package PAs.PA_1;
+
+public abstract class Media {
+    private String title, category;
+    private int year;
+    private long size;
+
+    protected Media() {
+        this("None", "None", 0, 0);
+    }
+
+    protected Media(String title, String category, int year, long size) {
+        this.title = title;
+        this.category = category;
+        this.year = year;
+        this.size = size;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public String getCategory() {
+        return category;
+    }
+
+    public int getYear() {
+        return year;
+    }
+
+    public long getSize() {
+        return size;
+    }
+
+    public String toString() {
+        return String.format("%-35s\t%-15s\t%-4d\t%-10d", title, category, year, size);
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public void setCategory(String category) {
+        this.category = category;
+    }
+
+    public void setYear(int year) {
+        this.year = year;
+    }
+
+    public void setSize(long size) {
+        this.size = size;
+    }
+}
