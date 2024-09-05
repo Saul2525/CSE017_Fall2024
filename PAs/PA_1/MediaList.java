44172f32028d61be36e3a72746feecf553d9a905
Saul Toribio <sat727@lehigh.edu>
2024-09-05 18:29:04 -0400

    What message?

--- PAs/PA_1/MediaList.java
+++ PAs/PA_1/MediaList.java
@@ -5,7 +5,35 @@
     IDE: VSCode; JDK: 11
 */
 
-public class MediaList extends Media {
-    private Media[] media = new Media[10];
+public class MediaList {
+    private Media[] media;
     private int count;
+
+    public MediaList() {
+        media = new Media[10];
+        count = 0;
+    }
+
+    public boolean add(Media media) {
+        if (count < 9) {
+            this.media[count] = media;
+            count++;
+
+            return true;
+        }
+
+        return false;
+    }
+
+    public Media findTitle(String title) {
+        return null;
+    }
+
+    public Media findCategory(String category) {
+        return null;
+    }
+
+    public Media findYear(int year) {
+        return null;
+    }
 }
