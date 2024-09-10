e46edfa9c3715c563bf31a79a493de7bf65006be
Saul Toribio <sat727@lehigh.edu>
2024-09-09 20:47:55 -0400

    What message?

--- PAs/PA_1/Media.java
+++ PAs/PA_1/Media.java
@@ -5,15 +5,29 @@
     IDE: VSCode; JDK: 11
 */
 
+/**
+ * A abstract class representing the tool used to deliver content.
+*/
 public abstract class Media {
     private String title, category;
     private int year;
     private long size;
 
+    /**
+     * Creates a media object. Default configuration.
+    */
     protected Media() {
         this("None", "None", 0, 0);
     }
 
+    /**
+     * Creates a media object with the specified title, category, year, and size.
+     * 
+     * @param title The media object's name.
+     * @param category The media object's genre.
+     * @param year The media object's year of release.
+     * @param size The media object's size in KB.
+    */
     protected Media(String title, String category, int year, long size) {
         this.title = title;
         this.category = category;
@@ -21,38 +35,73 @@ public abstract class Media {
         this.size = size;
     }
 
+    /**
+     * Gets the name of the media object.
+     * @return A string representing the name of the media object.
+    */
     public String getTitle() {
         return title;
     }
 
+    /**
+     * Gets the genre of the media object.
+     * @return A string representing the genre of the media object.
+    */
     public String getCategory() {
         return category;
     }
 
+    /**
+     * Gets the year of the media object.
+     * @return A int representing the year of the media object.
+    */
     public int getYear() {
         return year;
     }
 
+    /**
+     * Gets the size of the media object.
+     * @return A long representing the size of the media object in KB.
+    */
     public long getSize() {
         return size;
     }
 
+    /**
+     * @return A formated String containing the title, category, year, and size.
+    */
     public String toString() {
         return String.format("%-35s\t%-15s\t%-4d\t%-10d", title, category, year, size);
     }
 
+    /**
+     * Sets the name of the media object.
+     * @param title A string representing the name of the media object.
+    */
     public void setTitle(String title) {
         this.title = title;
     }
 
+    /**
+     * Sets the genre of the media object.
+     * @param category A string representing the genre of the media object.
+    */
     public void setCategory(String category) {
         this.category = category;
     }
 
+    /**
+     * Sets the year of the media object.
+     * @param year A int representing the year of the media object.
+    */
     public void setYear(int year) {
         this.year = year;
     }
 
+    /**
+     * Sets the size of the media object.
+     * @param size A long representing the size of the media object in KB.
+    */
     public void setSize(long size) {
         this.size = size;
     }
--- PAs/PA_1/MediaList.java
+++ PAs/PA_1/MediaList.java
@@ -5,15 +5,26 @@
     IDE: VSCode; JDK: 11
 */
 
+/**
+ * A class storing a set of media objects from the class Media.
+*/
 public class MediaList {
     private Media[] media;
     private int count;
 
+    /**
+     * Creates a media list. Default configuration.
+    */
     public MediaList() {
         media = new Media[10];
         count = 0;
     }
 
+    /**
+     * Adds a media object to the media list.
+     * @param media The media object. Either a movie or a song.
+     * @return True if the media object was added to media list. False otherwise.
+    */
     public boolean add(Media media) {
         if (count < 10) {
             this.media[count++] = media;
@@ -23,6 +34,11 @@ public class MediaList {
         return false;
     }
 
+    /**
+     * Returns a media object after matching the inputted title.
+     * @param title The title to be matched with a media object within the media list.
+     * @return The media object that matched the inputted title. Null otherwise.
+    */
     public Media findTitle(String title) {
         for (int i = 0; i < count; i++) {
             if (media[i].getTitle().equals(title)) {
@@ -33,6 +49,11 @@ public class MediaList {
         return null;
     }
 
+    /**
+     * Returns a media list matching the inputted category.
+     * @param category The category to be matched with media objects of the same category within the media list.
+     * @return A media list containing media objects with the matching category. Null otherwise.
+    */
     public Media[] findCategory(String category) {
         int categoryCount = 0;
 
@@ -59,6 +80,11 @@ public class MediaList {
         return null;
     }
 
+    /**
+     * Returns a media list matching the inputted year.
+     * @param year The year to be matched with media objects of the same year within the media list.
+     * @return A media list containing media objects with the matching year. Null otherwise.
+    */
     public Media[] findYear(int year) {
         int yearCount = 0;
 
@@ -85,11 +111,15 @@ public class MediaList {
         return null;
     }
 
+    /**
+     * Removes a media object from the media list.
+     * @param title The title to be matched with a media object within the media list.
+     * @return True if the media object was removed from the media list. False otherwise.
+    */
     public boolean remove(String title) {
         if (count > 0) {
             for (int k = 0; k < count; k++) {
                 if (media[k].getTitle().equals(title)) {
-
                     for (int j = k; j < count - 1; j++) {
                         media[j] = media[j + 1];
                     }
@@ -104,6 +134,10 @@ public class MediaList {
         return false;
     }
 
+    /**
+     * Sorts and prints the media list by name or by category.
+     * @param sortType True to sort the media list by name. False to sort it by category.
+    */
     public void sort(boolean sortType) {
         if (sortType) {
             for (int i = 0; i < count; i++) {
@@ -134,10 +168,18 @@ public class MediaList {
         }
     }
 
+    /**
+     * Returns the amount of media objects within the media list.
+     * @return What I said above. Or basically it just returns the parameter count.
+    */
     public int size() {
         return count;
     }
 
+    /**
+     * Combines the content of the media list into one string.
+     * @return A string that contains the content of the media list.
+    */
     public String toString() {
         System.out.println("Type\tTitle\t\t\t\t\t\t\t\tCategory\t\tYear\tSize(KB)\tArtist/Director\tRevenue");
         String tempString = "";
