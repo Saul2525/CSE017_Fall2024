dc13648e7cc019255d98f616eca44d907544724c
Saul Toribio <sat727@lehigh.edu>
2024-10-18 16:26:55 -0400

    What message?

--- ALAs/ALA_6/PrintRequest.java
+++ ALAs/ALA_6/PrintRequest.java
@@ -10,6 +10,12 @@ public class PrintRequest implements Comparable<PrintRequest> {
     private long size;
     private int id;
 
+    public PrintRequest() {
+        group = "";
+        size = 0;
+        id = 0;
+    }
+
     public PrintRequest(String group, long size, int id) {
         this.group = group;
         this.size = size;
@@ -41,7 +47,21 @@ public class PrintRequest implements Comparable<PrintRequest> {
     }
 
     public String toString() {
-        return String.format("%-10d\t%-5s\t%-10d", id, group, size);
+        String out = "";
+        if (size > 1000000000) {
+            size /= 1000000000.0;
+            out = size + "GB";
+        } else if (size > 1000000) {
+            size /= 1000000.0;
+            out = size + "MB";
+        } else if (size > 1000) {
+            size /= 1000.0;
+            out = size + "KB";
+        } else {
+            out = size + "B";
+        }
+
+        return String.format("%-10d\t%-10s\t%-10s", id, group, out);
     }
 
     private int getIndex() {
@@ -60,5 +80,16 @@ public class PrintRequest implements Comparable<PrintRequest> {
         int g2 = pr.getIndex();
 
         return g1 - g2;
+    }
+
+    public int getPriority() {
+        String[] priorities = {"root", "admin", "user", "batch"};
+        for (int i = 0; i < priorities.length; i++) {
+            if (group.equals(priorities[i])) {
+                return i;
+            }
+        }
+
+        return -1;
     }
 }
--- ALAs/ALA_6/Test.java
+++ ALAs/ALA_6/Test.java
@@ -5,60 +5,79 @@
     IDE: VSCode; JDK: 11
 */
 
+import java.io.File;
+import java.io.FileNotFoundException;
 import java.util.NoSuchElementException;
 import java.util.PriorityQueue;
 import java.util.Scanner;
 import java.util.Stack;
-import java.io.File;
-import java.io.FileNotFoundException;
 
 public class Test {
     public static void main(String[] args) {
         Scanner scnr = new Scanner(System.in);
-        Stack<Integer> postfixStack = new Stack<>();
 
-        try {
-            System.out.println("Enter a postfix expression:");
-            String postfix = scnr.nextLine();
-            String[] tokens = postfix.split(" ");
+        boolean runProgram = true;
+        do {
+            try {
+                Stack<Integer> postfixStack = new Stack<>();
+                System.out.println("Enter a postfix expression:");
+                String postfix = scnr.nextLine();
+                String[] tokens = postfix.split(" ");
 
-            for (String token : tokens) {
-                if (token.matches("\\d{1,}")) {
-                    int operand = Integer.parseInt(token);
-                    postfixStack.push(operand);
-                } else {
-                    int operand1 = postfixStack.pop();
-                    int operand2 = postfixStack.pop();
+                for (String token : tokens) {
+                    if (token.matches("\\d{1,}")) {
+                        int operand = Integer.parseInt(token);
+                        postfixStack.push(operand);
+                    } else {
+                        int operand1 = postfixStack.pop();
+                        int operand2 = postfixStack.pop();
 
-                    switch (token) {
-                        case "+":
-                            postfixStack.push(operand1 + operand2);
-                            break;
-                        case "-":
-                            postfixStack.push(operand2 - operand1);
-                            break;
-                        case "*":
-                            postfixStack.push(operand1 * operand2);
-                            break;
-                        case "/":
-                            postfixStack.push(operand2 / operand1);
-                            break;
-                        default:
-                            System.out.println("Invalid operation");
-                            break;
+                        switch (token) {
+                            case "+":
+                                postfixStack.push(operand1 + operand2);
+                                break;
+                            case "-":
+                                postfixStack.push(operand2 - operand1);
+                                break;
+                            case "*":
+                                postfixStack.push(operand1 * operand2);
+                                break;
+                            case "/":
+                                postfixStack.push(operand2 / operand1);
+                                break;
+                            default:
+                                System.out.println("Invalid operation");
+                                break;
+                        }
                     }
                 }
+
+                int result = postfixStack.pop();
+                if (!(postfixStack.isEmpty())) {
+                    System.out.println("Malformed expression.");
+                } else {
+                    System.out.printf("Result: %d\n", result);
+                }
+            } catch (NoSuchElementException e) {
+                System.out.println("Malformed expression.");
             }
 
-            int result = postfixStack.pop();
-            if (!(postfixStack.isEmpty())) {
-                System.out.println("Malformed expression");
-            } else {
-                System.out.printf("Result = %d\n", result);
+            System.out.println("Do you want to evaluate another postfix expression? (Yes/No):");
+            String selection = scnr.nextLine();
+            switch (selection) {
+                case "Yes":
+                case "yes":
+                case "Y":
+                case "y":
+                    break;
+                case "No":
+                case "no":
+                case "N":
+                case "n":
+                    runProgram = false;
+                    break;
             }
-        } catch (NoSuchElementException e) {
-            System.out.println("Malformed expression");
-        }
+        } while (runProgram == true);
 
         PriorityQueue<PrintRequest> printingQueue = new PriorityQueue<>();
         try {
@@ -73,14 +92,31 @@ public class Test {
             }
             readFile.close();
 
+            long totalTime = 0;
+            System.out.println("\nSummary of the printed requests:");
+            System.out.println("-------------------------------------------------------------");
+            System.out.println("User ID\t\tGroup\t\tSize\t\tCompletion Time");
+
             while (!printingQueue.isEmpty()) {
                 PrintRequest pr = printingQueue.poll();
                 long time = pr.getSize() / 10000;
-                System.out.printf("%s\t%d\n", pr, time);
+                totalTime += time;
+                System.out.printf("%s\t%s\n", pr, formatTime(time));
             }
+
+            System.out.println("\nTotal Printing Time: " + formatTime(totalTime));
         } catch (FileNotFoundException e) {
             System.out.println("File not found.");
         }
         scnr.close();
+    }
+
+    public static String formatTime(long time) {
+        long hours = (time / 3600000);
+        long minutes = ((time % 3600000) / 60000);
+        long seconds = ((time % 60000) / 1000);
+        long milliseconds = (time % 1000);
+
+        return String.format("%02d:%02d:%02d:%02d", hours, minutes, seconds, milliseconds);
     }
 }
